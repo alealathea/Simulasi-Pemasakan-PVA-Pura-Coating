a814614aec362cd09ac3163f90584fd9d130b23a
alealathea <alealathea08@gmail.com>
2026-03-04 09:07:04 +0700

    Update Prediction Tool.py

--- Prediction Tool.py
+++ Prediction Tool.py
@@ -121,8 +121,8 @@ def calapp (mtota,mpvaa,Tinit,bukaan1,bukaan2,deltasteam,Tslip,Tevap,xsteam,UA,m
 st.set_page_config(page_title="Simulator PVA", page_icon="Logo Pura No Text.png", layout="wide")
 
 st.image("Logo Pura Group.png", width=250)
-st.title("⚙️ Simulator Pemasakan PVA")
-st.markdown("Aplikasi Prediksi Waktu, Total Air Akhir, dan Kadar Solid PVA.")
+st.title("⚙️ PVA-CAST")
+st.markdown("PVA COOKING ANALYSIS AND SIMULATION TOOL")
 st.sidebar.image("Logo Pura Group.png", width=200)
 st.sidebar.markdown("---")
 st.sidebar.write("Research and Development")
@@ -273,6 +273,7 @@ if st.button('🚀 Jalankan Simulasi', use_container_width=True):
         st.error(f"❌ Terjadi kesalahan perhitungan: {e}")
 
         st.warning("Pastikan Anda sudah mem-paste fungsi 'calapp' dengan benar di bagian atas kode.")
+
 
 
 
